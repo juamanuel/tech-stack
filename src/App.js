e2a97ec704a54acaf7d300b2b81572684b216843
Juan Manuel Lopez <juanlg@me.com>
2019-06-30 15:11:55 -0500

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Header } from './components/common';
 
-const App = () => (
-        <View><Text>Hola Arkus</Text></View>
+const App = () => {
+    return (
+        <Provider store={createStore(reducers)}>
+            <View>
+                <Header title="Stack Tech" />
+            </View>
+        </Provider>
     );
+}
 
 export default App;
